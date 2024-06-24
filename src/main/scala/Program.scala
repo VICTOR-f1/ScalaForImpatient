450c5c01841bb8a13d7addfd4114a1b2d16b5976
ffvit <ffvit@DESKTOP-G0H593H>
2024-06-24 19:55:20 +0500

    Закончил первую главу

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -1,13 +1,18 @@
 object Program extends App {
   val objChapter1: Chapter1 = new Chapter1()
   objChapter1.exercise6()
+  objChapter1.exercise9()
 
 }
 
 class Chapter1 {
 
   def exercise6(): Unit = {
-    println(BigInt(2).pow(1025))
+    println(BigInt(2).pow(1024))
   }
 
+  def exercise9(): Unit = {
+    val str = "Scala"
+    println(str(0) + " " + str(str.length - 1))
+  }
 }
